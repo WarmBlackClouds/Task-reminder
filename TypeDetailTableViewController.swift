227bbf91dd6283a77bb3a79300103e5f8608bcfd
Guus <chunnanzi@me.com>
2017-10-01 00:29:02 +0800

    Create TypeDetailTableViewController.swift
    实现点击按钮 加入全局变量todoModel的分类数组
    增加分类的方法

---
+++ TypeDetailTableViewController.swift
@@ -0,0 +1,41 @@
+//
+//  TypeDetailTableViewController.swift
+//  todo
+//
+//  Created by 许 振辉 on 2017/9/20.
+//  Copyright © 2017年 许 振辉. All rights reserved.
+//
+
+import UIKit
+
+
+class TypeDetailTableViewController: UITableViewController ,ProtocolIconView,UITextFieldDelegate{
+
+    @IBAction func cancel(_ sender: Any) {
+        //界面跳转
+        self.tabBarController?.selectedIndex = 0
+        //还原成添加状态
+        onAddType()
+    }
+    @IBAction func done(_ sender: Any) {
+        //获取分类的名称
+        typeItem.name = textField.text!
+        
+        todoModel.onAddType(type: typeItem)
+         
+        //界面跳转 ps:tabBar视图可以通过底部进行界面跳转，只需要改变
+        //selfctedIndex的索引值就行了，0就是第一个的分类是视图，1则为添加视图
+        self.tabBarController?.selectedIndex = 0
+        
+        
+    }
+    
+    func onAddType(){
+        typeList.append(type)
+    }
+    
+    @IBOutlet weak var doneButton: UIBarButtonItem!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var iconImageView: UIImageView!
+    
+}
