dc475e0a434e746361e4f082e5ba6442dcdb2cff
Guus <chunnanzi@me.com>
2017-10-01 00:10:42 +0800

    Create TypeItem.swift
    Model

---
+++ TypeItem.swift
@@ -0,0 +1,24 @@
+//
+//  TypeItem.swift
+//  todo
+//
+//  Created by 许 振辉 on 2017/9/20.
+//  Copyright © 2017年 许 振辉. All rights reserved.
+//
+
+import UIKit
+
+class TypeItem: NSObject, NSCoding{
+    //任务分类的名称
+    var name :String? = ""
+    //分类图标
+    var icon: String? = "little_animal_06"
+    
+    
+    //构造方法
+    init(name:String) {
+        super.init()
+        self.name = name
+    }
+    
+}
