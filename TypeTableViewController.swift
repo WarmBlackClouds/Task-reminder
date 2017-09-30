6d99bc6ec0212bc032b824513b36c7236b8d27c0
Guus <chunnanzi@me.com>
2017-10-01 00:15:00 +0800

    Update TypeTableViewController.swift
    声明一个变量用来存储任务分类的数据
    创建onCreateData方法来生成模拟数据
    然后对numberOfRowsInSection与cellForRowAt进行改造

--- TypeTableViewController.swift
+++ TypeTableViewController.swift
@@ -9,10 +9,22 @@
 import UIKit
 
 class TypeTableViewController: UITableViewController {
+    
+    //生成一个变量用来存储任务分类的数据、模拟数据
+    var typeList = [TypeItem]()
+    
+    //创建模拟数据
+    func onCreateData(){
+        for i in 1...10{
+            let name = "任务：\(i)"
+            let type =  TypeItem(name:name)
+            typeList.append(type)
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        onCreateData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,11 +34,15 @@ class TypeTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // 返回10条数据
-        return 10
+//        return 10
+        return todoModel.typeList.count
     }
     
     //设置cell
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        //获取对应的数据
+        let typeItem = todoModel.typeList[indexPath.row]
         
         //为cell设置Identifier
         let cellIdentifier = "typerCell"
@@ -35,9 +51,9 @@ class TypeTableViewController: UITableViewController {
 //        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
         
         //为cell设置标题
-        cell.textLabel!.text = "typeCell"
+        cell.textLabel!.text = typeItem.name
         //设置cell的缩略图
-        cell.imageView!.image = UIImage(named: "xxx")
+        cell.imageView!.image = UIImage(named: typeItem.icon!)
 
 
         return cell
