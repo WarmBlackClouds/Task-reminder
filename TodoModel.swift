7d71c53ef9a0036ed82b169a0f4fd4ef759af6f8
Guus <chunnanzi@me.com>
2017-10-02 12:25:08 +0800

    读取本地数据、数据本地化NSUserDefaults、任务列表排序
    loadData() 方法
    改造构造方法
    添加nextTodoItemId实现代码
    添加两个排序方法

--- TodoModel.swift
+++ TodoModel.swift
@@ -13,11 +13,11 @@ class TodoModel: NSObject{
     //生成一个变量用来存储任务分类的数据、模拟数据
     var typeList = [TypeItem]()
     
-    //构造方法
     override init() {
         super.init()
         //初始化模拟数据
-        onCreateData()
+//        onCreateData()
+        loadData() 
         print("沙盒文件夹路径：\(documentsDirectory())")
         print("数据文件路径：\(dataFilePath())")
     }
@@ -36,9 +36,17 @@ class TodoModel: NSObject{
         }
     }
     
+    //获得itemid
+    //数据本地化之NSUserDefaults
     class func nextTodoItemId() -> Int{
-        
-        return 0
+        let userDefaults = UserDefaults.standard
+        //获得ChecklistItemId
+        let itemID = userDefaults.integer(forKey: "todoItemId")
+        //+1后保存
+        userDefaults.set(itemID+1, forKey: "todoItemId")
+        //强制要求userDefaults立即保存
+        userDefaults.synchronize()
+        return itemID
     }
     
     
@@ -66,6 +74,34 @@ class TodoModel: NSObject{
         //数据写入
         data.write(toFile: dataFilePath(), atomically: true)
         
+    }
+    
+    //读取数据
+    func loadData() {
+        //获取本地数据文件地址
+        let path = self.dataFilePath()
+        //声明文件管理器
+        let defaultManager = FileManager()
+        //通过文件地址判断数据文件是否存在
+        if defaultManager.fileExists(atPath: path) {
+            let url = URL(fileURLWithPath: path)
+            let data = try! Data(contentsOf: url)
+            //解码器
+            let unarchiver = NSKeyedUnarchiver(forReadingWith: data)
+            //通过归档时设置的关键字Checklist还原lists
+            typeList = unarchiver.decodeObject(forKey: "todolist") as! Array
+            //结束解码
+            unarchiver.finishDecoding()
+        }
+    }
+    
+    //对lists进行排序
+    func sortLists(){
+        typeList.sort(by: onSort)
+    }
+    
+    func onSort(s1:TypeItem,s2:TypeItem) -> Bool{
+        return s1.name! > s2.name!
     }
 }
 
