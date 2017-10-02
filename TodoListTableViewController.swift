0c170593701659b5646f8a4e6cdc13c68021d618
Guus <chunnanzi@me.com>
2017-10-02 11:11:37 +0800

    Update TodoListTableViewController.swift
    定一个  var todolist:TypeItem?  属性来存储任务清单数据
    设置行数与cell内容

--- TodoListTableViewController.swift
+++ TodoListTableViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class TodoListTableViewController: UITableViewController ,ProtocolTodoDetail{
-
+    
+    //tableView的数据
+    var todolist:TypeItem?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,4 +25,19 @@ class TodoListTableViewController: UITableViewController ,ProtocolTodoDetail{
         // Dispose of any resources that can be recreated.
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return (todolist!.items.count)
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //获取cell对应的数据
+        let item = todolist?.items[indexPath.row]
+        //获取cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "todoCell", for: indexPath)
+
+        //设置标题内容
+        cell.textLabel?.text = item?.text
+
+        return cell
+    }
 }
