86886509fd199410f1157d81e3d1d89178b0a34a
Guus <chunnanzi@me.com>
2017-10-02 12:33:40 +0800

    本地消息通知

--- TodoItem.swift
+++ TodoItem.swift
@@ -57,4 +57,57 @@ class TodoItem: NSObject, NSCoding {
         aCoder.encode(itemID,forKey:"ItemID")
         aCoder.encode(level,forKey:"level")
     }
+    
+    //发送通知消息
+    func scheduleNotification(){
+        //通过itemID获取已有的消息推送，然后删掉，以便重新判断
+        let existingNotification = self.notificationForThisItem() as! UILocalNotification?
+        //如果existingNotification不为nil就取消推送
+        if existingNotification != nil{
+            UIApplication.shared.cancelLocalNotification(existingNotification!)
+        }
+        //拿dueDate和NSDAte.date比较，结果是OrderedAscending，表示
+        //dueDate的时间比当前的早，已经过期，不需要提醒
+        //NSOrderedDescending保存的dueDate比当前时间晚
+        //NSOrdereSame 跟当前时间相同
+        if self.shouldRemind! && (self.dueDate?.compare(Date()) != ComparisonResult.orderedAscending){
+            print("通知通知！！！")
+            //创建UILocalNotification 来进行本地消息通知
+            var localNotification = UILocalNotification()
+            //推送时间
+            localNotification.fireDate = self.dueDate as! Date
+            //时区
+            localNotification.timeZone = NSTimeZone.default
+            //推送内容
+            localNotification.alertBody = self.text
+            //声音
+            localNotification.soundName = UILocalNotificationDefaultSoundName
+            //额外信息
+            localNotification.userInfo = ["ItemID":self.itemID]
+            
+            UIApplication.shared.scheduleLocalNotification(localNotification)
+        }
+    }
+    
+    //通过遍历所有消息推送，通过itemid的对比返回UILocalNotification
+    func notificationForThisItem() -> UILocalNotification?{
+        let allNotifications = UIApplication.shared.scheduledLocalNotifications
+        for notification in allNotifications!{
+            var info:Dictionary<String,Int>? = notification.userInfo as? Dictionary
+            var number = info?["ItemID"]
+            if number != nil && number == self.itemID{
+                return notification as? UILocalNotification
+            }
+        }
+        return nil
+    }
+    
+    //析构
+    deinit {
+        //删除任务消息推送
+        let existingNotification = self.notificationForThisItem() as! UILocalNotification?
+        if existingNotification != nil{
+            UIApplication.shared.cancelLocalNotification(existingNotification!)
+        }
+    }
 }
