481981f65a4e6ca1a6738235c413ddee6d583d2a
Guus <chunnanzi@me.com>
2017-10-02 11:53:53 +0800

    选择提醒时间
    添加时间选择器的显示与隐藏
    在didSelectRowAt选择实现
    设置cellForRowAt
    手动实现加入选择器之后会产生的高度变化heightForRowAt
    手动实现cell indentationLevelForRowAt
    改造numberOfRowsInSection

--- TodoDetailTableViewController.swift
+++ TodoDetailTableViewController.swift
@@ -47,7 +47,10 @@ class TodoDetailTableViewController: UITableViewController,ProtocolLevel,UITextF
     
     //定义一个协议
     var delegate : ProtocolTodoDetail?
-
+    
+    //声明一个变量来记录日期选择器的显示状态
+    var datePickerVisible:Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -69,6 +72,103 @@ class TodoDetailTableViewController: UITableViewController,ProtocolLevel,UITextF
         formatter.dateFormat = "yyyy年MM月dd日 HH:mm:ss"
         self.dueDateLabel.text = formatter.string(from: todoItem.dueDate! as Date)
     }
+    
+    //显示日期选择器
+    func showDatePicker(){
+        //状态打开
+        datePickerVisible = true
+        let indexPathDatePicker = NSIndexPath(row: 3, section: 1)
+        self.tableView.insertRows(at: [indexPathDatePicker as IndexPath], with: .automatic)
+    }
+    
+    
+    //隐藏日期选择器
+    func hideDatePicker(){
+        if datePickerVisible{
+            //状态设为关闭
+            datePickerVisible = false
+            let indexPathDatePicker = NSIndexPath(row: 3, section: 1)
+            self.tableView.deleteRows(at: [indexPathDatePicker as IndexPath], with: .fade)
+        }
+    }
+    
+    //选择cell的row后
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.tableView.deselectRow(at: indexPath, animated: true)
+        textField.resignFirstResponder()
+        //当执行到日期选择器上一行的时候，可以判断是否要显示日期选择器了
+        if indexPath.section == 1 && indexPath.row == 2{
+            if !datePickerVisible{
+                self.showDatePicker()
+            }else{
+                self.hideDatePicker()
+            }
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //因为日期选择器的位置在日期显示下面。它的位置就是第二个section和第三个row
+        if indexPath.section == 1 && indexPath.row == 3 {
+            //用重用的方式获取表示为DatePickerCell的cell
+            var cell = tableView.dequeueReusableCell(withIdentifier: "DatePickerCell") 
+            //如果没有就创建一个
+            if cell == nil{
+                //创建一个表示为DatePickerCell的cell
+                cell = UITableViewCell(style: .default, reuseIdentifier: "DatePickerCell")
+                //设置cell样式
+                cell?.selectionStyle = UITableViewCellSelectionStyle.none
+                //创建日期选择器
+                let datePicker = UIDatePicker(frame: CGRect(x: 0.0, y: 0.0, width: 320.0, height: 216.0))
+                //给日期选择器的tag
+                datePicker.tag = 100
+                //将日期选择器加入cell
+                cell?.contentView.addSubview(datePicker)
+                //注意：action里面的方法名后面需要加个冒号"："
+                datePicker.addTarget(self, action: #selector(self.dateChanged), for: UIControlEvents.valueChanged)
+            }
+            return cell!
+        }else{
+            return super.tableView(tableView, cellForRowAt: indexPath)
+        }
+        
+    }
+    //日期选择器响应方法
+    func dateChanged(datePicker:UIDatePicker){
+        //改变dueDate
+        todoItem.dueDate = datePicker.date as NSDate
+        //更新提醒时间文本框
+        upDateDueDateLabel()
+    }
+    //因为日期选择器插入后会引起cell高度的变化，所以要重新设置
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        //当渲染到日期选择器所在的cell的时候
+        if indexPath.section == 1 && indexPath.row == 3 {
+            return 217.0
+        }else{
+            return super.tableView(tableView, heightForRowAt: indexPath)
+        }
+    }
+
+    //当覆盖了静态的cell数据源方法时需要提供一个代理方法。因为数据源对新加进来的日期选择器的cell一无所知，所以要使用这个代理方法
+    override func tableView(_ tableView: UITableView, indentationLevelForRowAt indexPath: IndexPath) -> Int {
+        if indexPath.section == 1 && indexPath.row == 3 {
+            //当执行到日期选择器所在的indexpath就创建一个indexPath然后强插
+            let newIndexPath = NSIndexPath(row: 0, section: indexPath.section)
+            return super.tableView(tableView, indentationLevelForRowAt: newIndexPath as IndexPath)
+        }else{
+            return super.tableView(tableView, indentationLevelForRowAt: indexPath)
+        }
+    }
+    
+    //根据日期选择器日期的隐藏与否决定返回的row数量
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 1 && datePickerVisible{
+            return 4
+        }else{
+            return super.tableView(tableView, numberOfRowsInSection: section)
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
